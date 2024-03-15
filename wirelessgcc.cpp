b05239834dd0296bf9108efd12dfab2d04620515
AndyVotava <andycvotava@gmail.com>
2024-03-15 18:05:03 -0500

    redid readgcc

--- wirelessgcc.cpp
+++ wirelessgcc.cpp
@@ -5,18 +5,20 @@ int main() {
 
     uint sm = pio_claim_unused_sm(pio0, true);
 
-    GCconsole console1(1);
     GCcontroller controller1(0);
+    GCconsole console1(1);
+
     GCreport origin = controller1.getorigin();
+    busy_wait_ms(3);
     GCreport report = controller1.getreport();
     
     while(true){
 
-        if(console1.write(origin, report)){     //update report when console.write returns true
-            report = controller1.getreport();
+        if(console1.write(origin, report)){
+        report = controller1.getreport(); 
         }
+        
     }
-
     return 0;
 
 }
