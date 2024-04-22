c24721a4aaef7823219b22e589a5af659a67905e
Andy Votava <andycvotava@gmail.com>
2024-04-21 21:40:54 -0500

    removed generated file

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Andy/Documents/wirelessGCC
+# Install script for directory: C:/Users/Andy/Documents/GCCcommunication
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Andy/Documents/wirelessGCC/build/pico-sdk/cmake_install.cmake")
+  include("C:/Users/Andy/Documents/GCCcommunication/build/pico-sdk/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Andy/Documents/wirelessGCC/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Andy/Documents/GCCcommunication/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
