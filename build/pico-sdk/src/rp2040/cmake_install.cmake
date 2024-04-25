fccc069d46e00aaea2dd8f596bbbbdc17f7dc639
Andy Votava <andycvotava@gmail.com>
2024-04-25 13:16:01 -0500

    better naming

--- build/pico-sdk/src/rp2040/cmake_install.cmake
+++ build/pico-sdk/src/rp2040/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/wirelessgcc_project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/GCCcoms_project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
