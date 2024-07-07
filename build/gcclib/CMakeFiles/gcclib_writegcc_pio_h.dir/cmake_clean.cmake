17b4ce7ee5c9469590397dad2120c943eda5d2c6
Andy Votava <andycvotava@gmail.com>
2024-07-07 16:42:02 -0500

    Removed folder from repository

--- build/gcclib/CMakeFiles/gcclib_writegcc_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gcclib_writegcc_pio_h"
-  "writegcc.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gcclib_writegcc_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
